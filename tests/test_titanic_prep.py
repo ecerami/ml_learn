42e0d32ddb2824a31d943d9d96fb44bf37ebeae7
Ethan Cerami <cerami@gmail.com>
2021-06-22 14:18:12 -0400

    Added titatic prep and tests

---
+++ tests/test_titanic_prep.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from ml.titanic.titanic_prep import TitanicPrep
+
+
+def test_titanic_prep():
+    """Test Titatic Data Prep."""
+    df = pd.read_csv("data/titanic_train.csv")
+    training_X = df.drop("Survived", axis=1)
+    prep = TitanicPrep()
+    prep.fit(training_X)
+    training_X_prepared = prep.transform(training_X)
+
+    # Verify number of new columns
+    assert len(training_X_prepared.columns) == 12
+
+    # Verify that one hot encoding worked on the zeroeth row
+    row0 = training_X_prepared.iloc[0]
+    assert row0.Embarked_C == 0
+    assert row0.Embarked_Q == 0
+    assert row0.Embarked_S == 1
+
+    assert row0.Class_1 == 0
+    assert row0.Class_2 == 0
+    assert row0.Class_3 == 1
+
+    assert row0.Sex_female == 0
+    assert row0.Sex_male == 1
+
+    # Verify MinMax Scaling
+    assert training_X_prepared["Age"].max() <= 1
+    assert training_X_prepared["Age"].min() >= 0
+    assert training_X_prepared["Fare"].max() <= 1
+    assert training_X_prepared["Fare"].min() >= 0
