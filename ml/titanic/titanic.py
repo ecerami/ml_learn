1662368758679fa8409db79cbc61f0aedc583a7c
Ethan Cerami <cerami@gmail.com>
2021-06-23 10:55:50 -0400

    Continued to work on predictions

--- ml/titanic/titanic.py
+++ ml/titanic/titanic.py
@@ -1,14 +1,13 @@
 """Run the Titanic Classification Pipeline."""
+from ml.titanic.titanic_prep import TitanicPrep
 import pandas as pd
-
-# from sklearn.impute import SimpleImputer
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.metrics import accuracy_score
-# from sklearn.preprocessing import OneHotEncoder
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.compose import ColumnTransformer
-# from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 
 
 class TitanicPipeline:
@@ -17,8 +16,54 @@ class TitanicPipeline:
 
     def execute_pipeline(self):
         print("Loading Titanic Training Data Set.")
-        df = pd.read_csv("data/titanic_train.csv")
-        print(f"Loaded data frame: {df.shape[0]} x {df.shape[1]}.")
-        # y = df["Survived"]
-        X = df.drop("Survived", axis=1)
-        print(X.head())
+        train_df = pd.read_csv("data/titanic_train.csv")
+        print(f"Loaded data frame: {train_df.shape[0]} x {train_df.shape[1]}.")
+        train_y = train_df["Survived"]
+        train_X = train_df.drop("Survived", axis=1)
+
+        print("Transforming training set.")
+        prep = TitanicPrep()
+        prep.fit(train_X)
+        train_X = prep.transform(train_X)
+
+        knn = KNeighborsClassifier()
+        self.assess_model("KNeighbors", knn, train_X, train_y)
+
+        sgd = SGDClassifier()
+        self.assess_model("SGD", sgd, train_X, train_y)
+
+        gnb = GaussianNB()
+        self.assess_model("Naive Bayes", gnb, train_X, train_y)
+
+        svc = SVC()
+        self.assess_model("Support Vector", svc, train_X, train_y)
+
+        ada = AdaBoostClassifier()
+        self.assess_model("Ada Boost", ada, train_X, train_y)
+
+        rfc = RandomForestClassifier()
+        self.assess_model("Random Forest", rfc, train_X, train_y)
+
+        print("Predicting on test data set.")
+        test_df = pd.read_csv("data/titanic_test.csv")
+        passengerIds = test_df["PassengerId"]
+        test_X = prep.transform(test_df)
+        svc.fit(train_X, train_y)
+        survived = svc.predict(test_X)
+        submission_df = pd.DataFrame()
+        submission_df["PassengerId"] = passengerIds
+        submission_df["Surived"] = survived
+        out_name = "out/titanic_predictions.csv"
+        print (f"Writing predicions to {out_name}.")
+        submission_df.to_csv(out_name, sep=",", index=False)
+
+    def assess_model(self, name, model, train_X, train_y):
+        results = cross_val_score(
+            model,
+            train_X,
+            train_y,
+            cv=3,
+            verbose=0,
+            scoring="accuracy",
+        )
+        print(f"{name} = {results.mean():.4f}")
--- ml/titanic/titanic_prep.py
+++ ml/titanic/titanic_prep.py
@@ -8,36 +8,34 @@ from sklearn.pipeline import Pipeline
 
 
 class TitanicPrep:
+    """Prepare the Titanic Data Set."""
+
     drop_columns = ["PassengerId", "Name", "Ticket", "Cabin"]
-    num_columns = ["SibSp", "Parch", "Fare"]
-    cat_columns = ["Pclass", "Sex"]
+    num_columns = ["SibSp", "Parch", "Age", "Fare"]
+    cat_columns = ["Embarked", "Pclass", "Sex"]
 
     def __init__(self):
         """Construct prep pipeline."""
 
     def fit(self, training_df):
-        age_pipeline = Pipeline(
+        impute_scale = Pipeline(
             [
-                ("impute_age", SimpleImputer(strategy="median")),
+                ("impute", SimpleImputer(strategy="median")),
                 ("scale", MinMaxScaler()),
             ]
         )
-        embark_pipeline = Pipeline(
+        impute_ohe = Pipeline(
             [
-                ("impute_embark", SimpleImputer(strategy="most_frequent")),
+                ("impute", SimpleImputer(strategy="most_frequent")),
                 ("ohe", OneHotEncoder()),
             ]
         )
-        num_pipeline = Pipeline([("scale", MinMaxScaler())])
-        cat_pipeline = Pipeline([("ohe", OneHotEncoder())])
 
         self.pipeline = ColumnTransformer(
             [
                 ("drop", "drop", self.drop_columns),
-                ("embark", embark_pipeline, ["Embarked"]),
-                ("cat", cat_pipeline, self.cat_columns),
-                ("num", num_pipeline, self.num_columns),
-                ("age", age_pipeline, ["Age"]),
+                ("impute_ohe", impute_ohe, self.cat_columns),
+                ("impute_scale", impute_scale, self.num_columns),
             ]
         )
         self.pipeline.fit(training_df)
@@ -45,18 +43,14 @@ class TitanicPrep:
     def transform(self, X):
         new_X = pd.DataFrame(self.pipeline.transform(X))
 
-        # Hack to get all the new column names
-        emb_list = self.pipeline.transformers_[1][1][1].get_feature_names()
-        cat_list = self.pipeline.transformers_[2][1][0].get_feature_names()
-        num_list = self.num_columns
-        emb_list = [x.replace("x0", "Embarked") for x in emb_list]
-        cat_list = [x.replace("x0", "Class") for x in cat_list]
-        cat_list = [x.replace("x1", "Sex") for x in cat_list]
+        # Hack to get all the new OHE column names
+        cat_list = self.pipeline.transformers_[1][1][1].get_feature_names()
+        cat_list = [x.replace("x0", "Embarked") for x in cat_list]
+        cat_list = [x.replace("x1", "Class") for x in cat_list]
+        cat_list = [x.replace("x2", "Sex") for x in cat_list]
 
         new_columns = []
-        new_columns.extend(emb_list)
         new_columns.extend(cat_list)
-        new_columns.extend(num_list)
-        new_columns.append("Age")
+        new_columns.extend(self.num_columns)
         new_X.columns = new_columns
         return new_X
