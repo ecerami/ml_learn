3bdeb9fd1b9647ea4291a01cd2ca2b9acd2b4900
Ethan Cerami <cerami@gmail.com>
2021-07-08 12:11:58 -0400

    Replaced getter scripts with Makefile

--- data/get_mnist.sh
+++
@@ -1,2 +0,0 @@
-wget https://www.openml.org/data/get_csv/52667/mnist_784.arff
-mv mnist_784.arff mnist_784.csv
--- data/get_spam.sh
+++
@@ -1,5 +0,0 @@
-wget http://spamassassin.apache.org/old/publiccorpus/20021010_easy_ham.tar.bz2
-wget http://spamassassin.apache.org/old/publiccorpus/20021010_spam.tar.bz2
-bzip2 -d *.tar.bz2
-tar -xvf 20021010_spam.tar
-tar -xvf 20021010_easy_ham.tar
